4cfc838dedeeeb8f68cff5acce0a1cd3b65e845d
Roman Gutovec <roman.gutovec@gmail.com>
2019-01-15 14:51:36 +0300

    Merge step9. Add method SetStatus to HiveService and HiveSectionService classes

--- KatlaSport.Services/HiveManagement/HiveSectionService.cs
+++ KatlaSport.Services/HiveManagement/HiveSectionService.cs
@@ -59,7 +59,21 @@ namespace KatlaSport.Services.HiveManagement
         /// <inheritdoc/>
         public async Task SetStatusAsync(int hiveSectionId, bool deletedStatus)
         {
-            throw new NotImplementedException();
+            var dbSections = await _context.Sections.Where(c => hiveSectionId == c.Id).ToArrayAsync();
+
+            if (dbSections.Length == 0)
+            {
+                throw new RequestedResourceNotFoundException();
+            }
+
+            var dbHiveSection = dbSections[0];
+            if (dbHiveSection.IsDeleted != deletedStatus)
+            {
+                dbHiveSection.IsDeleted = deletedStatus;
+                dbHiveSection.LastUpdated = DateTime.UtcNow;
+                dbHiveSection.LastUpdatedBy = _userContext.UserId;
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
